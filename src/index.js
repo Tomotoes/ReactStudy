59bdc0f2c1b652e215d6e1f989796a497249459a
Simon Ma <jinmaup@gmail.com>
2018-05-31 14:37:20 +0800

    test
    test

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ const styles = {
 	fontFamily: 'sans-serif',
 	textAlign: 'center'
 }
-const Message = ({ name }) => <h2>I am {name}</h2>
+
 const App = () => (
 	<div style={styles}>
 		<Hello name="CodeSandbox" />
-		<Message name="Simon" />
 		<h2>Start editing to see some magic happen {'\u2728'}</h2>
 	</div>
 )
